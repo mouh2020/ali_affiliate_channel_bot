aad6711a5bfc43bfc5abf0096d846b4c47fde173
mouh2020 <badimohammed2019@gmail.com>
2023-10-06 23:09:51 +0100

    add tracking_id

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
-interval    = "" # Time to wait between shares in minutes.
-channel_id  = "" # The channel_ID (Extract it from the web)
-bot_token   = "" # The bot token (Create one from BOT FATHER)
-app_key     = "" # App Key of AliExpress API
-app_secret  = "" # App Secret of AliExpress API
+interval    = ""        # Time to wait between shares in minutes.
+channel_id  = ""        # The channel_ID (Extract it from the web)
+bot_token   = ""        # The bot token (Create one from BOT FATHER)
+app_key     = ""        # App Key of AliExpress API
+app_secret  = ""        # App Secret of AliExpress API
+tracking_id = "default" # Tracking_ID from portals (default as default)
