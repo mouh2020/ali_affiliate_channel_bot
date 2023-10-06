18863bd775f313626209fae628b51650a73697e2
mouh2020 <badimohammed2019@gmail.com>
2023-10-06 22:58:44 +0100

    first commit

---
+++ config.py
@@ -0,0 +1,5 @@
+interval    = "" # Time to wait between shares in minutes.
+channel_id  = "" # The channel_ID (Extract it from the web)
+bot_token   = "" # The bot token (Create one from BOT FATHER)
+app_key     = "" # App Key of AliExpress API
+app_secret  = "" # App Secret of AliExpress API
