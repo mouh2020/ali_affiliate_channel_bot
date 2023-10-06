1440839947014081708e671e47c4194df1f48718
mouh2020 <badimohammed2019@gmail.com>
2023-10-06 23:43:37 +0100

    add categories

--- config.py
+++ config.py
@@ -4,3 +4,4 @@ bot_token   = ""        # The bot token (Create one from BOT FATHER)
 app_key     = ""        # App Key of AliExpress API
 app_secret  = ""        # App Secret of AliExpress API
 tracking_id = "default" # Tracking_ID from portals (default as default)
+categories  = "1,42"    # Choose the numbers of categories from categories.json file (In this example we choose Food and Hardware)
