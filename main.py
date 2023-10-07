1c71599275e72c763d37f5e907a58156f355c6b6
mouh2020 <badimohammed2019@gmail.com>
2023-10-07 13:11:43 +0100

    first commit

--- main.py
+++ main.py
@@ -9,16 +9,20 @@ logger.add("bot.log",format="{time:YYYY-MM-DD HH:mm:ss} | {level} | {function} |
 
 bot = TeleBot(bot_token)
 
-
-for index,_ in enumerate(range(50),start=1) :
-    products = get_products(page_no=index)
-    for product in products : 
-        if product_exsits(product_id=product.product_id) : 
-            continue
-        insert_product(product_id=product.product_id)
-        product = build_affiliate_link(product)
-        message = build_message(product)
-        bot.send_photo(chat_id=channel_id,
-                       photo=product.product_main_image_url,
-                       caption=message)
-        sleep(int(interval))
+try :
+    for index,_ in enumerate(range(50),start=1) :
+        products = get_products(page_no=index)
+        for product in products : 
+            if product_exsits(product_id=product.product_id) : 
+                continue
+            insert_product(product_id=product.product_id)
+            product = build_affiliate_link(product)
+            message = build_message(product)
+            bot.send_photo(chat_id=channel_id,
+                        photo=product.product_main_image_url,
+                        caption=message)
+            logger.info(f"product shared successfully product_id : {product.product_id}")
+            logger.info(f"sleep for {interval} minutes for next share")
+            sleep(int(interval)*60)
+except Exception as e : 
+    logger.error(f"an error occured : {str(e)}")
