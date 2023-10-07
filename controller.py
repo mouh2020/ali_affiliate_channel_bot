19588e76c8ef8a3909325113c87024f25e7edd51
mouh2020 <badimohammed2019@gmail.com>
2023-10-07 01:03:53 +0100

    add logger

--- controller.py
+++ controller.py
@@ -1,14 +1,21 @@
 from tinydb import TinyDB,Query
+from loguru import logger
+
+logger.add("bot.log",format="{time:YYYY-MM-DD HH:mm:ss} | {level} | {function} | {message}",colorize=False,enqueue=True,mode="a")
 
 db = TinyDB("database.json")
 
 def product_exsits(product_id) : 
+    logger.info(f"check existance of product_id : {product_id}")
     fetched_product = Query()
     product_existance = db.search(fetched_product.id == product_id)
     if len(product_existance) != 0 : 
+        logger.info(f"product exists product_id : {product_id}")
         return True
+    logger.info(f"there is no product_id : {product_id}")
     
 def insert_product(product_id) : 
+    logger.info(f"insert product to databse product_id : {product_id}")
     new_product = {'id' : product_id}
     db.insert(new_product)     
 
